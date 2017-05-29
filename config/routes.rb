0f94c92f2ab5d7a948e71f9cdc6b529d1db8fbc6
shijunfeng <johnshiluck@gmail.comgit>
2017-05-29 10:39:19 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
+  resources :jobs do
+    resources :resumes
+  end
   root 'jobs#index'
 
   namespace :admin do
     resources :jobs do
       member do
         post :publish
-        post :hide 
+        post :hide
       end
     end
   end
